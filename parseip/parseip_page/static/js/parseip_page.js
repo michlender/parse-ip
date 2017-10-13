6dcb7759dff4c222227fedf8071fc2494f559b4e
Michele Collender <michelecollender@Micheles-MacBook-Pro.local>
2017-10-13 13:58:08 -0700

    A start with jQuery

--- parseip/parseip_page/static/js/parseip_page.js
+++ parseip/parseip_page/static/js/parseip_page.js
@@ -1,3 +1,14 @@
 // When user clicks button, grab input
 // Run that input through python method
-// Display the results back on the page 
+// Display the results back on the page
+
+
+var handleClick = function() {
+  var input = $( 'input' );
+  var request = $.post(url="/ajax/parseip_page/", format="json", data=input);
+  request.done(function(data) {
+    $( '<textarea id="returnedParse">data</textarea>' );
+  });
+}
+
+$( 'button' ).on('click', handleClick);
