87e4b0a26ed132f6092e743d51c3863efe44d842
Michele Collender <michelecollender@Micheles-MacBook-Pro.local>
2017-10-20 14:25:44 -0700

    Should be 403 free

--- parseip/parseip_page/views.py
+++ parseip/parseip_page/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 def parseip_page(request):
     return render(request, 'parseip_page/parseip_page.html', {})
 
-
+@csrf_exempt
 def parse(request):
     # may have to convert request from json to dict? check
     # process (don't worry about this yet)
