964b6c74a9a40a2a49f3db4cb2607c3c30890a9f
Michele Collender <michelecollender@Micheles-MacBook-Pro.local>
2017-12-07 13:46:07 -0800

    Finding ip addresses before validating.

--- parseip/parseip_page/parse-ip.py
+++ parseip/parseip_page/parse-ip.py
@@ -2,28 +2,24 @@
 import re
 
 toReturn = ''
-#address_range = '^(12[0-7]|1[01]\d|\d\d|\d|0?\d?\d)$'
-#address_range = '^(0?\d?\d|1[01]\d|12[0-7])$'
-#address_range = '^(\d?\d|1[01]\d|12[0-7])$'
-#address_range = '^(\d|\d\d|1[01]\d|12[0-7])$'
-#address_range = '^(\d?\d?\d|1[01]\d|12[0-7])$'
-address_range = '^(0?[0-9]?[0-9]|1[01][0-9]|12[0-7])$'
+address_catch_ipv4 = '(\d+\.*){4}'
+
 
 def find_valid_ip(input):
-    #valid_ip = re.compile('^[0-127].[0-127].[0-127].[0-127]$')
-    #valid_ip = re.compile('^\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}$')
-    #valid_ip = '^(\d|\d\d|1[01]\d|12[0-7].\d|\d\d|1[01]\d|12[0-7].\d|\d\d|1[01]\d|12[0-7].\d|\d\d|1[01]\d|12[0-7])$'
-    valid_ip = re.compile('^(' + address_range + '.' + address_range + '.' + address_range + '.' + address_range + ')$')
-    found_ip = valid_ip.search(input)
+    possible_ips = re.compile(address_catch_ipv4)
+    found_ips = re.findall(possible_ips, input)
 
-    if found_ip != None:
-        toReturn += found_ips.group()
-    else:
-        toReturn = 'No IP addresses found.'
+    for ips in found_ips:
+        print(ips)
+
+    #if found_ips != None:
+        #toReturn += found_ips.group()
+    #else:
+        #toReturn = 'No IP addresses found.'
 
     #input = input[found_ip.end():]
 
-    print(toReturn)
+    #print(toReturn)
     #if is_in_range(input):
     #    toReturn += '-'
     #else:
@@ -34,5 +30,5 @@ def is_in_range(input):
 
 #def parse_ip(input):
 
-find_valid_ip("from 127.0.0.1")
-find_valid_ip("127")
+find_valid_ip('from 127.0.0.1')
+find_valid_ip("127.0.0.9")
