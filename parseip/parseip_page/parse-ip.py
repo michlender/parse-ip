1109ad7db987d6b0cc4e0a9bf9b94db71a78bc15
Michele Collender <michelecollender@Micheles-MacBook-Pro.local>
2017-12-18 14:57:03 -0800

    Added regex for finding range of ips

--- parseip/parseip_page/parse-ip.py
+++ parseip/parseip_page/parse-ip.py
@@ -4,21 +4,27 @@ import re, ipaddress
 toReturn = ''
 #address_catch_ipv4 = '((\d+\.*){4})'
 address_catch_ipv4 = '((\d{1,3}\.){3}\d{1,3})'
+delimiter = '(-|~|[Tt][Oo])'
 
 
 def find_valid_ip(input):
-    possible_ips = re.compile(address_catch_ipv4)
-    found_ips = re.findall(possible_ips, input)
+    individual_ip = re.compile(address_catch_ipv4)
+    all_ips = re.findall(individual_ip, input)
+    ips_in_range = is_in_range(input)
+    print(all_ips)
 
-    for ips in found_ips:
-        print('Caught the address: ' + ips[0])
-        if (validate_ip(ips[0])):
-            return True
+
+    for ips in all_ips:
+        print(ips)
+        #print('Caught the address: ' + ips[0])
+        #if (validate_ip(ips[0])):
+            #return True
             #is_in_range
+
 
 def validate_ip(address):
     try:
-        ipaddress.ip_address(address)
+        ipaddress.IPv4Address(address)
         print(address + ' is valid.')
         return True
     except ValueError:
@@ -26,28 +32,15 @@ def validate_ip(address):
         return False
 
 def is_in_range(input):
-    # regular expression
-    delimiter = re.compile('-|~|[Tt][Oo]')
+    address_range = re.compile(address_catch_ipv4 + '\s*' + delimiter + '\s*' + address_catch_ipv4)
+    address_ranges = re.findall(address_range, input)
+
+    for address in address_ranges:
+        print(address)
 
 
-
-    #if found_ips != None:
-        #toReturn += found_ips.group()
-    #else:
-        #toReturn = 'No IP addresses found.'
-
-    #input = input[found_ip.end():]
-
-    #print(toReturn)
-    #if is_in_range(input):
-    #    toReturn += '-'
-    #else:
-    #    toReturn += ' '
-
-
-
-#def parse_ip(input):
-
-find_valid_ip('from 127.0.0.1')
-find_valid_ip("127.0.0.9")
-find_valid_ip('999.999.999.999')
+# find_valid_ip('from 127.0.0.1')
+# find_valid_ip("127.0.0.9")
+# find_valid_ip('999.999.999.999')
+# find_valid_ip('from 127.0.0.1 to 127.0.0.2')
+find_valid_ip('from 127.0.0.1~ 127.0.0.2 127.0.0.3')
