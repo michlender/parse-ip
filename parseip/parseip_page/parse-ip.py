77e83efe453094a0e52bcb29072ee1206e538c68
Michele Collender <michelecollender@Micheles-MacBook-Pro.local>
2017-12-26 15:27:16 -0800

    Removed unnecessary regex catches and fixed validation step.

--- parseip/parseip_page/parse-ip.py
+++ parseip/parseip_page/parse-ip.py
@@ -1,11 +1,8 @@
 
 import re, ipaddress
 
-toReturn = ''
-#address_catch_ipv4 = '((\d+\.*){4})'
-address_catch_ipv4 = '((\d{1,3}\.){3}\d{1,3})'
+address_catch_ipv4 = '((?:\d{1,3}\.){3}\d{1,3})'
 delimiter = '(-|~|[Tt][Oo])'
-
 
 def find_valid_ip(input):
     individual_ip = re.compile(address_catch_ipv4)
@@ -13,22 +10,20 @@ def find_valid_ip(input):
     ips_in_range = is_in_range(input)
     print(all_ips)
 
+    for ip in all_ips:
+        print(ip)
+        if not validate_ip(ip):
+            all_ips.remove(ip)
+            print(all_ips)
 
-    for ips in all_ips:
-        print(ips)
-        #print('Caught the address: ' + ips[0])
-        #if (validate_ip(ips[0])):
-            #return True
-            #is_in_range
+    if not any(for range_ip in ips_in_range):
 
 
 def validate_ip(address):
     try:
         ipaddress.IPv4Address(address)
-        print(address + ' is valid.')
         return True
     except ValueError:
-        print('Invalid IP address.')
         return False
 
 def is_in_range(input):
@@ -41,6 +36,7 @@ def is_in_range(input):
 
 # find_valid_ip('from 127.0.0.1')
 # find_valid_ip("127.0.0.9")
-# find_valid_ip('999.999.999.999')
+find_valid_ip('999.999.999.999')
+find_valid_ip('127.0.0.3 999.999.999.999')
 # find_valid_ip('from 127.0.0.1 to 127.0.0.2')
 find_valid_ip('from 127.0.0.1~ 127.0.0.2 127.0.0.3')
