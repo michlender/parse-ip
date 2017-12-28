cc5ae310ffd59b7137826a50988eef46a25dcb02
Michele Collender <michelecollender@Micheles-MacBook-Pro.local>
2017-12-27 18:07:10 -0800

    Writing IPs to string

--- parseip/parseip_page/parse-ip.py
+++ parseip/parseip_page/parse-ip.py
@@ -1,22 +1,38 @@
 
 import re, ipaddress
 
+
 address_catch_ipv4 = '((?:\d{1,3}\.){3}\d{1,3})'
-delimiter = '(-|~|[Tt][Oo])'
+delimiter = '(?:-|~|[Tt][Oo])'
 
 def find_valid_ip(input):
     individual_ip = re.compile(address_catch_ipv4)
     all_ips = re.findall(individual_ip, input)
     ips_in_range = is_in_range(input)
-    print(all_ips)
 
     for ip in all_ips:
-        print(ip)
         if not validate_ip(ip):
             all_ips.remove(ip)
-            print(all_ips)
 
-    if not any(for range_ip in ips_in_range):
+        #if not any(range_ip in ranges for ranges in ips_in_range):
+        for ranges in ips_in_range:
+            if not any(ip in range_ip for range_ip in ranges):
+                print('cool')
+            else:
+                print('uncool')
+
+    toReturn = ''
+    print(ips_in_range)
+    for ip in all_ips:
+        for ranges in ips_in_range:
+            if ip in ranges:
+                range_index = all_ips.index(ip)
+                toReturn += all_ips.pop(range_index) + '-' + all_ips.pop(range_index)
+                print(toReturn)
+                print(all_ips)
+        toReturn += ' ' + all_ips.pop(0)
+        print(toReturn)
+    return toReturn
 
 
 def validate_ip(address):
@@ -33,10 +49,12 @@ def is_in_range(input):
     for address in address_ranges:
         print(address)
 
+    return address_ranges
+
 
 # find_valid_ip('from 127.0.0.1')
 # find_valid_ip("127.0.0.9")
-find_valid_ip('999.999.999.999')
-find_valid_ip('127.0.0.3 999.999.999.999')
+#find_valid_ip('999.999.999.999')
+#find_valid_ip('127.0.0.3 999.999.999.999')
 # find_valid_ip('from 127.0.0.1 to 127.0.0.2')
-find_valid_ip('from 127.0.0.1~ 127.0.0.2 127.0.0.3')
+find_valid_ip('from 127.0.0.1~ 127.0.0.2 127.0.0.3 999.999.999.999')
